b177db1666bc925a8b2e0205eb517dbdd7cfdc9a
Maryam MoneyWali <maryammoneywali@gmail.com>
2025-06-13 16:36:08 +0500

    index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include 'styles.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Payment Platform</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to PayClone</h1>
+        <p>The easiest way to send and receive payments.</p>
+        <button onclick="window.location.href='signup.php'">Sign Up</button>
+        <button onclick="window.location.href='login.php'">Login</button>
+    </div>
+</body>
+</html>
